e12d6bbd293224da36e7531b31d3e72afda6fcf7
Kyle <Kyle.Widmann@gmail.com>
2018-06-05 17:20:18 -0500

    Adding in updates for exercise notebooks

---
+++ intro-to-tensorflow/testTensorFlow.py
@@ -0,0 +1,39 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+# Import MNIST data
+mnist = input_data.read_data_sets('/datasets/ud730/mnist', one_hot=True)
+
+# The features are already scaled and the data is shuffled
+train_features = mnist.train.images
+test_features = mnist.test.images
+
+train_labels = mnist.train.labels.astype(np.float32)
+test_labels = mnist.test.labels.astype(np.float32)
+
+# Weights & bias
+weights = tf.Variable(tf.random_normal([n_input, n_classes]))
+bias = tf.Variable(tf.random_normal([n_classes]))
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+human_files_short = human_files[:100]
+dog_files_short = train_files[:100]
+
+for image in human_files_short:
+    if face_detector:
+        human_count = human_count +1
+
+for image in dog_files_short:
+    if face_detectory:
+        dog_count++
+
+print("% Human faces detected: {}".format(human_count/count(human_files_short)))
+print("% Faces detected in dog images: {}".format(dog_count/count(dog_files_short)))
